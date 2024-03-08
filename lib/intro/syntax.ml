19a2fac9c815ec75bd848ad88a236760f649693d
Henry Till <henrytill@gmail.com>
2024-03-07 21:26:34 -0800

    intro: add infix expression pretty-printer

--- lib/intro/syntax.ml
+++ lib/intro/syntax.ml
@@ -7,16 +7,27 @@ type t =
   | Mul of t * t
   | Exp of t * t
 
-let rec pp fmt x =
+let rec pp_ast fmt x =
   let open Format in
   match x with
   | Var a -> fprintf fmt "@[Var %S@]" a
   | Const a -> fprintf fmt "@[Const %d@]" a
-  | Neg a -> fprintf fmt "@[Neg (%a)@]" pp a
-  | Add (a, b) -> fprintf fmt "@[Add (%a, %a)@]" pp a pp b
-  | Sub (a, b) -> fprintf fmt "@[Sub (%a, %a)@]" pp a pp b
-  | Mul (a, b) -> fprintf fmt "@[Mul (%a, %a)@]" pp a pp b
-  | Exp (a, b) -> fprintf fmt "@[Exp (%a, %a)@]" pp a pp b
+  | Neg a -> fprintf fmt "@[Neg (%a)@]" pp_ast a
+  | Add (a, b) -> fprintf fmt "@[Add (%a, %a)@]" pp_ast a pp_ast b
+  | Sub (a, b) -> fprintf fmt "@[Sub (%a, %a)@]" pp_ast a pp_ast b
+  | Mul (a, b) -> fprintf fmt "@[Mul (%a, %a)@]" pp_ast a pp_ast b
+  | Exp (a, b) -> fprintf fmt "@[Exp (%a, %a)@]" pp_ast a pp_ast b
+
+let rec pp fmt x =
+  let open Format in
+  match x with
+  | Var a -> fprintf fmt "@[%s@]" a
+  | Const a -> fprintf fmt "@ %d@]" a
+  | Neg a -> fprintf fmt "@[- %a@]" pp a
+  | Add (a, b) -> fprintf fmt "@[(%a + %a)@]" pp a pp b
+  | Sub (a, b) -> fprintf fmt "@[(%a - %a)@]" pp a pp b
+  | Mul (a, b) -> fprintf fmt "@[(%a * %a)@]" pp a pp b
+  | Exp (a, b) -> fprintf fmt "@[(%a ^ %a)@]" pp a pp b
 
 let rec equal x y =
   match (x, y) with
