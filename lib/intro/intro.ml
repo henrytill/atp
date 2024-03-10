e9f8308cb2250a8acf06e3866344e9ef08412f0a
Henry Till <henrytill@gmail.com>
2024-03-10 12:50:07 -0700

    intro: rewrite parse_string in terms of parse, simplify parse

--- lib/intro/intro.ml
+++ lib/intro/intro.ml
@@ -1,12 +1,5 @@
 module Syntax = Syntax
 
-let parse (lexbuf : Lexing.lexbuf) : Syntax.t option =
-  let maybe_toplevel = Parser.toplevel Lexer.token lexbuf in
-  maybe_toplevel
-
-let parse_string (s : string) : Syntax.t option =
-  let lexbuf = Lexing.from_string s in
-  let maybe_toplevel = Parser.toplevel Lexer.token lexbuf in
-  maybe_toplevel
-
+let parse : Lexing.lexbuf -> Syntax.t option = Parser.toplevel Lexer.token
+let parse_string (s : string) : Syntax.t option = Lexing.from_string s |> parse
 let simplify = Semantics.simplify
