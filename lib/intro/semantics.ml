781b2d29ee75080f8046eac347dc18c2063a5396
Henry Till <henrytill@gmail.com>
2024-04-16 11:25:01 -0700

    Intro.Semantics: tweak

--- lib/intro/semantics.ml
+++ lib/intro/semantics.ml
@@ -35,29 +35,24 @@ let simplify_with_count (expr : Syntax.t) : Syntax.t * int =
   let count = ref 0 in
   let rec go (expr : Syntax.t) : Syntax.t =
     incr count;
-    let add x y = simplify1 count (Add (go x, go y)) in
-    let sub x y = simplify1 count (Sub (go x, go y)) in
-    let mul x y = simplify1 count (Mul (go x, go y)) in
-    let exp x y = simplify1 count (Exp (go x, go y)) in
-    let single x = simplify1 count (go x) in
-    let neg x = simplify1 count (Neg (go x)) in
+    let simply x = simplify1 count (go x) in
     match expr with
-    | Add (Const 0, x) | Add (x, Const 0) -> single x
-    | Add (x, y) -> add x y
-    | Sub (x, Const 0) -> single x
+    | Add (Const 0, x) | Add (x, Const 0) -> simply x
+    | Add (x, y) -> simplify1 count (Add (go x, go y))
+    | Sub (x, Const 0) -> simply x
     | Sub (x, y) when Syntax.equal x y -> Const 0
-    | Sub (x, y) -> sub x y
+    | Sub (x, y) -> simplify1 count (Sub (go x, go y))
     | Mul (Const 0, _) | Mul (_, Const 0) -> Const 0
-    | Mul (Const 1, x) | Mul (x, Const 1) -> single x
-    | Mul (x, y) -> mul x y
+    | Mul (Const 1, x) | Mul (x, Const 1) -> simply x
+    | Mul (x, y) -> simplify1 count (Mul (go x, go y))
     | Exp (_, Const 0) -> Const 1
     | Exp (Const 0, _) -> Const 0
     | Exp (Const 1, _) -> Const 1
-    | Exp (x, Const 1) -> single x
+    | Exp (x, Const 1) -> simply x
     | Exp (_, Neg (Const _)) -> raise (Invalid_argument err_raise_negative)
-    | Exp (x, y) -> exp x y
-    | Neg (Neg x) -> single x
-    | Neg x -> neg x
+    | Exp (x, y) -> simplify1 count (Exp (go x, go y))
+    | Neg (Neg x) -> simply x
+    | Neg x -> simplify1 count (Neg (go x))
     | Const _ | Var _ -> expr
   in
   let ret = go expr in
