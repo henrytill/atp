087d1ae50b5ee96bd72584f026d6a76bad91e281
Henry Till <henrytill@gmail.com>
2024-04-16 00:33:50 -0700

    Intro: update test names

--- test/Math/ATP/Intro/Quote/Tests.hs
+++ test/Math/ATP/Intro/Quote/Tests.hs
@@ -11,39 +11,39 @@ tests :: TestTree
 tests =
   testGroup
     "Quote"
-    [ testCase "a" $ do
+    [ testCase "Parse variable" $ do
         [intro| a |] @?= Var "a",
-      testCase "42" $ do
+      testCase "Parse constant" $ do
         [intro| 42 |] @?= Const 42,
-      testCase "42 + 42" $ do
+      testCase "Parse addition" $ do
         [intro| 42 + 42 |] @?= Add (Const 42) (Const 42),
-      testCase "42 * 42" $ do
+      testCase "Parse multiplication" $ do
         [intro| 42 * 42 |] @?= Mul (Const 42) (Const 42),
-      testCase "2 ^ 3" $ do
+      testCase "Parse exponentiation" $ do
         [intro| 2 ^ 3 |] @?= Exp (Const 2) (Const 3),
-      testCase "x - - - x" $ do
+      testCase "Parse subtraction with negation" $ do
         [intro| x - - - x |] @?= Sub (Var "x") (Neg (Neg (Var "x"))),
-      testCase "2 * x + y" $ do
+      testCase "Parse compound expression" $ do
         [intro| 2 * x + y |] @?= Add (Mul (Const 2) (Var "x")) (Var "y"),
-      testCase "(0 * x + 1) * 3 + 12" $ do
+      testCase "Parse example compound expression" $ do
         [intro| (0 * x + 1) * 3 + 12 |] @=? Add (Mul (Add (Mul (Const 0) (Var "x")) (Const 1)) (Const 3)) (Const 12),
-      testCase "$m - 1" $ do
+      testCase "Parse metavariable" $ do
         [intro| $m - 1 |] @?= Sub (Const 1) (Const 1),
-      testCase "1 + 2 * 3" $ do
+      testCase "Parse precedence (right)" $ do
         [intro| 1 + 2 * 3 |] @?= Add (Const 1) (Mul (Const 2) (Const 3)),
-      testCase "1 * 2 + 3" $ do
+      testCase "Parse precedence (left)" $ do
         [intro| 1 * 2 + 3 |] @?= Add (Mul (Const 1) (Const 2)) (Const 3),
-      testCase "x + y + z" $ do
+      testCase "Parse addition precedence" $ do
         [intro| x + y + z |] @?= Add (Add (Var "x") (Var "y")) (Var "z"),
-      testCase "x - y - z" $ do
+      testCase "Parse subtraction precedence" $ do
         [intro| x - y - z |] @?= Sub (Sub (Var "x") (Var "y")) (Var "z"),
-      testCase "x * y * z" $ do
+      testCase "Parse multiplication precedence" $ do
         [intro| x * y * z |] @?= Mul (Mul (Var "x") (Var "y")) (Var "z"),
-      testCase "x ^ y ^ z" $ do
+      testCase "Parse exponentiation precedence" $ do
         [intro| x ^ y ^ z |] @?= Exp (Var "x") (Exp (Var "y") (Var "z")),
-      testCase "(1 + 2) * 3" $ do
+      testCase "Parse parentheses (left)" $ do
         [intro| (1 + 2) * 3 |] @?= Mul (Add (Const 1) (Const 2)) (Const 3),
-      testCase "1 + (2 * 3)" $ do
+      testCase "Parse parentheses (right)" $ do
         [intro| 1 + (2 * 3) |] @?= Add (Const 1) (Mul (Const 2) (Const 3))
     ]
   where
--- test/Math/ATP/Intro/Semantics/Tests.hs
+++ test/Math/ATP/Intro/Semantics/Tests.hs
@@ -12,42 +12,42 @@ tests :: TestTree
 tests =
   testGroup
     "Semantics"
-    [ testCase "x" $ do
+    [ testCase "Simplify variable" $ do
         simplifyWithCount [intro| x |] @?= (Var "x", 1),
-      testCase "1 + 1" $ do
+      testCase "Simplify addition" $ do
         simplifyWithCount [intro| 1 + 1 |] @?= (Const 2, 4),
-      testCase "(1 + 2) * (3 + 4)" $ do
+      testCase "Simplify compound expression" $ do
         simplifyWithCount [intro| (1 + 2) * (3 + 4) |] @?= (Const 21, 10),
-      testCase "0 + x" $ do
+      testCase "Simplify add to 0 (left)" $ do
         simplifyWithCount [intro| 0 + x |] @?= (Var "x", 3),
-      testCase "x + 0" $ do
+      testCase "Simplify add to 0 (right)" $ do
         simplifyWithCount [intro| x + 0 |] @?= (Var "x", 3),
-      testCase "x - 0" $ do
+      testCase "Simplify subtract 0" $ do
         simplifyWithCount [intro| x - 0 |] @?= (Var "x", 3),
-      testCase "0 * x" $ do
+      testCase "Simplify multiply by 0 (left)" $ do
         simplifyWithCount [intro| 0 * x |] @?= (Const 0, 1),
-      testCase "x * 0" $ do
+      testCase "Simplify multiply by 0 (right)" $ do
         simplifyWithCount [intro| x * 0 |] @?= (Const 0, 1),
-      testCase "1 * x" $ do
+      testCase "Simplify multiply by 1 (left)" $ do
         simplifyWithCount [intro| 1 * x |] @?= (Var "x", 3),
-      testCase "x * 1" $ do
+      testCase "Simplify multiply by 1 (right)" $ do
         simplifyWithCount [intro| x * 1 |] @?= (Var "x", 3),
-      testCase "0 ^ x" $ do
+      testCase "Simplify 0 raised to x" $ do
         simplifyWithCount [intro| 0 ^ x |] @?= (Const 0, 1),
-      testCase "x ^ 0" $ do
+      testCase "Simplify x raised to 0" $ do
         simplifyWithCount [intro| x ^ 0 |] @?= (Const 1, 1),
-      testCase "0 ^ 0" $ do
+      testCase "Simplify 0 raised to 0" $ do
         simplifyWithCount [intro| 0 ^ 0 |] @?= (Const 1, 1),
-      testCase "1 ^ x" $ do
+      testCase "Simplify 1 raised to x" $ do
         simplifyWithCount [intro| 1 ^ x |] @?= (Const 1, 1),
-      testCase "x ^ 1" $ do
+      testCase "Simplify x raised to 1" $ do
         simplifyWithCount [intro| x ^ 1 |] @?= (Var "x", 3),
-      testCase "x - - - x" $ do
+      testCase "Simplify subtraction with negation" $ do
         simplifyWithCount [intro| x - - - x |] @?= (Const 0, 6),
-      testCase "0 + (0 + (1 - 1))" $ do
+      testCase "Simplify some short circuits (a)" $ do
         simplifyWithCount [intro| 0 + (0 + (1 - 1)) |] @?= (Const 0, 5),
-      testCase "- - 1 - 1" $ do
+      testCase "Simplify some short circuits (b)" $ do
         simplifyWithCount [intro| - - (1 - 1) |] @?= (Const 0, 3),
-      testCase "(0 * x + 1) * 3 + 12" $ do
+      testCase "Simplify example compound expression" $ do
         simplifyWithCount [intro| (0 * x + 1) * 3 + 12 |] @?= (Const 15, 10)
     ]
