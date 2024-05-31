c8d6a16259e0c21ffe856e97b4e9b86e7344e168
Henry Till <henrytill@gmail.com>
2024-05-31 13:53:44 -0700

    intro: tweak

--- lib/intro/lexer.mll
+++ lib/intro/lexer.mll
@@ -1,5 +1,5 @@
 {
-  open Parser
+open Parser
 }
 
 let int = (['0'-'9']+)
--- lib/intro/syntax.ml
+++ lib/intro/syntax.ml
@@ -7,11 +7,11 @@ type t =
   | Mul of t * t
   | Exp of t * t
 
-let pp_ast fmt x =
+let pp_ast fmt expr =
   let open Format in
   let rec go fmt = function
-    | Var a -> fprintf fmt "@[Var %S@]" a
-    | Const a -> fprintf fmt "@[Const %d@]" a
+    | Var x -> fprintf fmt "@[Var %S@]" x
+    | Const m -> fprintf fmt "@[Const %d@]" m
     | Neg a -> fprintf fmt "@[Neg (%a)@]" go a
     | Add (a, b) -> fprintf fmt "@[Add (%a, %a)@]" go a go b
     | Sub (a, b) -> fprintf fmt "@[Sub (%a, %a)@]" go a go b
@@ -19,27 +19,28 @@ let pp_ast fmt x =
     | Exp (a, b) -> fprintf fmt "@[Exp (%a, %a)@]" go a go b
   in
   fprintf fmt "@[(";
-  go fmt x;
+  go fmt expr;
   fprintf fmt ")@]"
 
-let rec pp fmt x =
+let rec pp fmt expr =
   let open Format in
-  match x with
-  | Var a -> fprintf fmt "@[%s@]" a
-  | Const a -> fprintf fmt "@[%d@]" a
+  match expr with
+  | Var x -> fprintf fmt "@[%s@]" x
+  | Const m -> fprintf fmt "@[%d@]" m
   | Neg a -> fprintf fmt "@[(- %a)@]" pp a
   | Add (a, b) -> fprintf fmt "@[(%a + %a)@]" pp a pp b
   | Sub (a, b) -> fprintf fmt "@[(%a - %a)@]" pp a pp b
   | Mul (a, b) -> fprintf fmt "@[(%a * %a)@]" pp a pp b
   | Exp (a, b) -> fprintf fmt "@[(%a ^ %a)@]" pp a pp b
 
-let rec equal x y =
-  match (x, y) with
-  | Var a, Var b -> String.equal a b
-  | Const a, Const b -> Int.equal a b
+let rec equal e1 e2 =
+  match (e1, e2) with
+  | Var x, Var y -> String.equal x y
+  | Const m, Const n -> Int.equal m n
   | Neg a, Neg b -> equal a b
-  | Add (a1, a2), Add (b1, b2) -> equal a1 b1 && equal a2 b2
-  | Sub (a1, a2), Sub (b1, b2) -> equal a1 b1 && equal a2 b2
-  | Mul (a1, a2), Mul (b1, b2) -> equal a1 b1 && equal a2 b2
-  | Exp (a1, a2), Exp (b1, b2) -> equal a1 b1 && equal a2 b2
+  | Add (a1, b1), Add (a2, b2)
+  | Sub (a1, b1), Sub (a2, b2)
+  | Mul (a1, b1), Mul (a2, b2)
+  | Exp (a1, b1), Exp (a2, b2) ->
+      equal a1 a2 && equal b1 b2
   | _, _ -> false
