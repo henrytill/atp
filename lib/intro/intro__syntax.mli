1a74210cf331b3b7cc2a2dd0659b8959271152e7
Henry Till <henrytill@gmail.com>
2024-08-14 21:10:21 -0700

    {Intro,Prop_logic}__syntax: add signatures

---
+++ lib/intro/intro__syntax.mli
@@ -0,0 +1,12 @@
+type t =
+  | Var of string
+  | Const of int
+  | Neg of t
+  | Add of t * t
+  | Sub of t * t
+  | Mul of t * t
+  | Exp of t * t
+
+val pp_ast : Format.formatter -> t -> unit
+val pp : Format.formatter -> t -> unit
+val equal : t -> t -> bool
---
+++ lib/prop_logic/prop_logic__syntax.mli
@@ -0,0 +1,33 @@
+module Prop : sig
+  type t
+
+  val inj : string -> t
+  val prj : t -> string
+  val pp_ast : Format.formatter -> t -> unit
+  val pp : Format.formatter -> t -> unit
+  val equal : t -> t -> bool
+end
+
+module Formula : sig
+  type 'a t =
+    | Atom of 'a
+    | False
+    | True
+    | Not of 'a t
+    | And of 'a t * 'a t
+    | Or of 'a t * 'a t
+    | Imp of 'a t * 'a t
+    | Iff of 'a t * 'a t
+    | Forall of string * 'a t
+    | Exists of string * 'a t
+
+  val pp_ast : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
+  val pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
+  val equal : ('a -> 'b -> bool) -> 'a t -> 'b t -> bool
+end
+
+type t = Prop.t Formula.t
+
+val pp_ast : Format.formatter -> t -> unit
+val pp : Format.formatter -> t -> unit
+val equal : t -> t -> bool
