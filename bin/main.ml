aaf622a499940108cd2bd809e07c95fde1eacea9
Henry Till <henrytill@gmail.com>
2024-08-16 17:18:02 -0700

    main: factor out common functions

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,43 @@
+open Format
+
 let print_exn formatter exn =
-  Format.pp_print_string formatter (Printexc.to_string exn);
-  Format.pp_print_newline formatter ()
+  pp_print_string formatter (Printexc.to_string exn);
+  pp_print_newline formatter ()
+
+let run_with_args read_eval_print anon_args out_formatter err_formatter =
+  try
+    List.iter
+      (fun a ->
+        let lexbuf = Lexing.from_string a in
+        ignore (read_eval_print lexbuf out_formatter);
+        pp_print_flush out_formatter ())
+      anon_args;
+    exit 0
+  with exn ->
+    print_exn err_formatter exn;
+    pp_print_flush err_formatter ();
+    exit 1
+
+let run_with_stdin read_eval_print out_formatter err_formatter =
+  let continue = ref true in
+  let lexbuf = Lexing.from_channel Stdlib.stdin in
+  try
+    while !continue do
+      continue := read_eval_print lexbuf out_formatter;
+      pp_print_flush out_formatter ()
+    done;
+    exit 0
+  with exn ->
+    print_exn err_formatter exn;
+    pp_print_flush err_formatter ();
+    exit 1
+
+let run read_eval_print anon_args out_formatter err_formatter =
+  let has_anon_args = List.length anon_args > 0 in
+  if has_anon_args then
+    run_with_args read_eval_print anon_args out_formatter err_formatter
+  else
+    run_with_stdin read_eval_print out_formatter err_formatter
 
 module type COMMAND = sig
   val speclist : (string * Arg.spec * string) list
@@ -18,70 +55,36 @@ module Command_intro : COMMAND = struct
       ("-count", Arg.Set show_count, "Show number of simplification steps");
     ]
 
-  let anon_args = ref []
-
   let print_ast formatter expr =
     Intro.Syntax.pp_ast formatter expr;
-    Format.pp_print_newline formatter ()
+    pp_print_newline formatter ()
 
   let print_expr formatter expr =
     Intro.Syntax.pp formatter expr;
-    Format.pp_print_newline formatter ()
+    pp_print_newline formatter ()
 
   let maybe_print_count formatter count =
     if !show_count then begin
-      Format.fprintf formatter "Steps: %a" Format.pp_print_int count;
-      Format.pp_print_newline formatter ()
+      fprintf formatter "Steps: %a" pp_print_int count;
+      pp_print_newline formatter ()
     end
 
   let read_eval_print lexbuf formatter =
-    let open Intro in
-    match Input.parse lexbuf with
+    match Intro.Input.parse lexbuf with
     | Some expr when !dump_ast ->
         print_ast formatter expr;
         true
     | Some expr ->
-        let simplified, count = Semantics.simplify_with_count expr in
+        let simplified, count = Intro.Semantics.simplify_with_count expr in
         print_expr formatter expr;
         print_expr formatter simplified;
         maybe_print_count formatter count;
         true
     | None -> false
 
+  let anon_args = ref []
   let anon_fun arg = anon_args := arg :: !anon_args
-
-  let run_args out_formatter err_formatter =
-    try
-      List.iter
-        (fun a ->
-          let lexbuf = Lexing.from_string a in
-          ignore (read_eval_print lexbuf out_formatter);
-          Format.pp_print_flush out_formatter ())
-        !anon_args;
-      exit 0
-    with exn ->
-      print_exn err_formatter exn;
-      Format.pp_print_flush err_formatter ();
-      exit 1
-
-  let run_stdin out_formatter err_formatter =
-    let continue = ref true in
-    let lexbuf = Lexing.from_channel Stdlib.stdin in
-    try
-      while !continue do
-        continue := read_eval_print lexbuf out_formatter;
-        Format.pp_print_flush out_formatter ()
-      done;
-      exit 0
-    with exn ->
-      print_exn err_formatter exn;
-      Format.pp_print_flush err_formatter ();
-      exit 1
-
-  let main () =
-    let has_anon_args = List.length !anon_args > 0 in
-    let f = if has_anon_args then run_args else run_stdin in
-    f Format.std_formatter Format.err_formatter
+  let main () = run read_eval_print !anon_args std_formatter err_formatter
 end
 
 module Command_prop_logic : COMMAND = struct
@@ -94,68 +97,34 @@ module Command_prop_logic : COMMAND = struct
       ("-dump-truthtable", Arg.Set dump_truthtable, "Dump truth table");
     ]
 
-  let anon_args = ref []
-
   let print_ast formatter fm =
     Prop_logic.Syntax.pp_ast formatter fm;
-    Format.pp_print_newline formatter ()
+    pp_print_newline formatter ()
 
   let print_formula formatter fm =
     Prop_logic.Syntax.pp formatter fm;
-    Format.pp_print_newline formatter ()
+    pp_print_newline formatter ()
 
   let read_eval_print lexbuf formatter =
-    let open Prop_logic in
-    match Input.parse lexbuf with
+    match Prop_logic.Input.parse lexbuf with
     | Some fm when !dump_ast && !dump_truthtable ->
         print_ast formatter fm;
-        Semantics.print_truthtable formatter fm;
+        Prop_logic.Semantics.print_truthtable formatter fm;
         true
     | Some fm when !dump_ast ->
         print_ast formatter fm;
         true
     | Some fm when !dump_truthtable ->
-        Semantics.print_truthtable formatter fm;
+        Prop_logic.Semantics.print_truthtable formatter fm;
         true
     | Some fm ->
         print_formula formatter fm;
         true
     | None -> false
 
+  let anon_args = ref []
   let anon_fun arg = anon_args := arg :: !anon_args
-
-  let run_args out_formatter err_formatter =
-    try
-      List.iter
-        (fun a ->
-          let lexbuf = Lexing.from_string a in
-          ignore (read_eval_print lexbuf out_formatter);
-          Format.pp_print_flush out_formatter ())
-        !anon_args;
-      exit 0
-    with exn ->
-      print_exn err_formatter exn;
-      Format.pp_print_flush err_formatter ();
-      exit 1
-
-  let run_stdin out_formatter err_formatter =
-    let continue = ref true in
-    let lexbuf = Lexing.from_channel Stdlib.stdin in
-    try
-      while !continue do
-        continue := read_eval_print lexbuf out_formatter;
-        Format.pp_print_flush out_formatter ()
-      done;
-      exit 0
-    with exn ->
-      print_exn err_formatter exn;
-      Format.pp_print_flush err_formatter ();
-      exit 1
-
-  let main () =
-    let has_anon_args = List.length !anon_args > 0 in
-    let f = if has_anon_args then run_args else run_stdin in
-    f Format.std_formatter Format.err_formatter
+  let main () = run read_eval_print !anon_args std_formatter err_formatter
 end
 
 let speclist = ref []
@@ -174,17 +143,17 @@ let select (arg : string) =
   | _ -> raise (Arg.Bad ("Unknown command: " ^ arg))
 
 let dispatch arg = !anon_fun arg
-let usage_msg = String.empty
+let usage_msg = "Usage: command [options] <arguments>"
 
 let () =
   anon_fun := select;
   begin
     try Arg.parse_argv_dynamic Sys.argv speclist dispatch usage_msg with
     | Arg.Bad msg ->
-        Printf.eprintf "%s" msg;
+        eprintf "%s" msg;
         exit 2
     | Arg.Help msg ->
-        Printf.printf "%s" msg;
+        printf "%s" msg;
         exit 0
   end;
   !main ()
