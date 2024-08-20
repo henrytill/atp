df33bf21e5d65accc0b171bc24e1a596befc8623
Henry Till <henrytill@gmail.com>
2024-08-20 00:35:37 -0700

    PropLogic.Semantics: add printTruthtable

--- src/PropLogic/Semantics.hs
+++ src/PropLogic/Semantics.hs
@@ -2,6 +2,8 @@ module PropLogic.Semantics where
 
 import Data.List qualified as List
 import PropLogic.Syntax
+import Text.PrettyPrint
+import Prelude hiding ((<>))
 
 eval :: Formula Prop -> (Prop -> Bool) -> Bool
 eval FmFalse _ = False
@@ -38,3 +40,37 @@ atomUnion f fm = setify $ overAtoms ((++) . f) fm []
 
 atoms :: (Ord a) => Formula a -> [a]
 atoms = atomUnion (: [])
+
+onAllValuations :: (Eq a) => ((a -> Bool) -> b) -> (a -> Bool) -> [a] -> [b]
+onAllValuations subfn v ats = case ats of
+  [] -> [subfn v]
+  p : ps ->
+    let v' t q = if q == p then t else v q
+     in onAllValuations subfn (v' False) ps ++ onAllValuations subfn (v' True) ps
+
+printTruthtable :: Formula Prop -> Doc
+printTruthtable fm = vcat $ header : separator : body
+  where
+    ats :: [Prop]
+    ats = atoms fm
+
+    width :: Int
+    width = foldr (max . length . unProp) 5 ats + 1
+
+    fixw :: String -> Doc
+    fixw s = text $ s ++ replicate (width - length s) ' '
+
+    header :: Doc
+    header = foldr ((<>) . fixw . unProp) (text "| formula") ats
+
+    separator :: Doc
+    separator = text $ replicate ((width * length ats) + 9) '-'
+
+    truthString :: Bool -> Doc
+    truthString = fixw . show
+
+    mkRow :: (Prop -> Bool) -> Doc
+    mkRow v = foldr ((<>) . truthString . v) (text "| " <> truthString (eval fm v)) ats
+
+    body :: [Doc]
+    body = onAllValuations mkRow (const False) ats
