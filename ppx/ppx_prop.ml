687d245b711aee2a2de04040828befa76f7edcd7
Henry Till <henrytill@gmail.com>
2024-12-08 21:18:23 -0800

    ppx_prop: tweak

--- ppx/ppx_prop.ml
+++ ppx/ppx_prop.ml
@@ -1,42 +1,46 @@
 open Ppxlib
 
-let atom_to_expr ~(loc : location) (p : Prop_logic.Syntax.Prop.t) : expression =
+let prop_to_expression ~(loc : location) (p : Prop_logic.Syntax.Prop.t) : expression =
   [%expr
     Prop_logic.Syntax.Formula.Atom
       (Prop_logic.Syntax.Prop.inj
          [%e Ast_builder.Default.estring ~loc (Prop_logic.Syntax.Prop.prj p)])]
 
-let rec formula_to_expr ~(loc : location) : Prop_logic.Syntax.t -> expression = function
-  | Atom p -> atom_to_expr ~loc p
+let rec syntax_to_expression ~(loc : location) : Prop_logic.Syntax.t -> expression = function
+  | Atom p -> prop_to_expression ~loc p
   | False -> [%expr Prop_logic.Syntax.Formula.False]
   | True -> [%expr Prop_logic.Syntax.Formula.True]
-  | Not p -> [%expr Prop_logic.Syntax.Formula.Not [%e formula_to_expr ~loc p]]
+  | Not p -> [%expr Prop_logic.Syntax.Formula.Not [%e syntax_to_expression ~loc p]]
   | And (p, q) ->
       [%expr
-        Prop_logic.Syntax.Formula.And ([%e formula_to_expr ~loc p], [%e formula_to_expr ~loc q])]
+        Prop_logic.Syntax.Formula.And
+          ([%e syntax_to_expression ~loc p], [%e syntax_to_expression ~loc q])]
   | Or (p, q) ->
       [%expr
-        Prop_logic.Syntax.Formula.Or ([%e formula_to_expr ~loc p], [%e formula_to_expr ~loc q])]
+        Prop_logic.Syntax.Formula.Or
+          ([%e syntax_to_expression ~loc p], [%e syntax_to_expression ~loc q])]
   | Imp (p, q) ->
       [%expr
-        Prop_logic.Syntax.Formula.Imp ([%e formula_to_expr ~loc p], [%e formula_to_expr ~loc q])]
+        Prop_logic.Syntax.Formula.Imp
+          ([%e syntax_to_expression ~loc p], [%e syntax_to_expression ~loc q])]
   | Iff (p, q) ->
       [%expr
-        Prop_logic.Syntax.Formula.Iff ([%e formula_to_expr ~loc p], [%e formula_to_expr ~loc q])]
+        Prop_logic.Syntax.Formula.Iff
+          ([%e syntax_to_expression ~loc p], [%e syntax_to_expression ~loc q])]
   | Forall (x, p) ->
       [%expr
         Prop_logic.Syntax.Formula.Forall
-          ([%e Ast_builder.Default.estring ~loc x], [%e formula_to_expr ~loc p])]
+          ([%e Ast_builder.Default.estring ~loc x], [%e syntax_to_expression ~loc p])]
   | Exists (x, p) ->
       [%expr
         Prop_logic.Syntax.Formula.Exists
-          ([%e Ast_builder.Default.estring ~loc x], [%e formula_to_expr ~loc p])]
+          ([%e Ast_builder.Default.estring ~loc x], [%e syntax_to_expression ~loc p])]
   | Metavar s -> Ast_builder.Default.evar ~loc s
 
 let expand ~loc ~path:_ s =
   match Prop_logic.Input.parse_string s with
-  | Some formula -> formula_to_expr ~loc formula
-  | None -> Location.raise_errorf ~loc "Failed to parse propositional formula"
+  | Some syntax -> syntax_to_expression ~loc syntax
+  | None -> Location.raise_errorf ~loc "Failed to parse prop"
 
 let extension =
   Extension.declare "prop" Extension.Context.expression
