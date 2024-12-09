a708cd5fa6884fc76d75f10274ea93b0ad579583
Henry Till <henrytill@gmail.com>
2024-12-09 00:10:01 -0800

    Ppx_prop_test: iterate

--- test/prop_logic/ppx_prop_test.ml
+++ test/prop_logic/ppx_prop_test.ml
@@ -1,25 +1,35 @@
 let is_equiv () =
-  let open Prop_logic in
-  let actual : Syntax.t = {%prop| (p ==> q) /\ (q ==> r) ==> (p ==> r) |} in
-  let expected = Input.parse_string {|(p ==> q) /\ (q ==> r) ==> (p ==> r)|} |> Option.get in
-  Syntax.equal expected actual
+  let actual = {%prop| (p ==> q) /\ (q ==> r) ==> (p ==> r) |} in
+  let expected =
+    Prop_logic.Input.parse_string {|(p ==> q) /\ (q ==> r) ==> (p ==> r)|} |> Option.get
+  in
+  Prop_logic.Syntax.equal expected actual
 
 let meta_works () =
-  let open Prop_logic in
-  let x : Syntax.t = {%prop| p \/ q |} in
-  let actual : Syntax.t = {%prop| p /\ $x |} in
-  let expected = Input.parse_string {|p /\ (p \/ q)|} |> Option.get in
-  Syntax.equal expected actual
+  let x = {%prop| p \/ q |} in
+  let actual = {%prop| p /\ $x |} in
+  let expected = Prop_logic.Input.parse_string {|p /\ (p \/ q)|} |> Option.get in
+  Prop_logic.Syntax.equal expected actual
+
+let meta_fn_works () =
+  let make x = {%prop| p /\ $x |} in
+  let input = {%prop| p \/ q |} in
+  let expected = Prop_logic.Input.parse_string {|p /\ (p \/ q)|} |> Option.get in
+  Prop_logic.Syntax.equal expected (make input)
 
 let () =
-  let tests = [| ("is_equiv", is_equiv); ("meta_works", meta_works) |] in
+  let tests =
+    [| ("is_equiv", is_equiv); ("meta_works", meta_works); ("meta_fn_works", meta_fn_works) |]
+  in
+  let ret = ref 0 in
   for i = 0 to Array.length tests - 1 do
     let name, f = tests.(i) in
     print_string ("Testing " ^ name ^ ": ");
-    let result = f () in
-    if result then
+    let passed = f () in
+    if passed then
       print_endline "PASSED"
     else (
       print_endline "FAILED";
-      exit 1)
-  done
+      ret := 1)
+  done;
+  exit !ret
