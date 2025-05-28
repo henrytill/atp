586a4efc58fd8a4d9fec25acb4bb99442270484f
Henry Till <henrytill@gmail.com>
2025-05-27 20:12:28 -0700

    Intro_test: continue revamping

--- test/intro/intro_test.ml
+++ test/intro/intro_test.ml
@@ -3,7 +3,7 @@ open Intro
 let syntax = Alcotest.testable Syntax.pp_ast Syntax.equal
 let same_expr = "same expression"
 
-module Test_parse = struct
+module Test_parse_string = struct
   let inputs : (Syntax.t * string) list =
     [
       (Var "a", {| a |});
@@ -25,14 +25,14 @@ module Test_parse = struct
       (Mul (Const 1, Add (Const 2, Const 3)), {| 1 * (2 + 3) |});
     ]
 
-  let generate (inputs : (Syntax.t * string) list) =
+  let generate ?(is = inputs) () =
     let f (output, input) =
       let expected = Some output in
       let actual = Input.parse_string input in
       let test () = Alcotest.(check (option syntax)) same_expr expected actual in
       Alcotest.(test_case ("Parse " ^ String.trim input) `Quick test)
     in
-    List.map f inputs
+    List.map f is
 end
 
 module Test_simplify = struct
@@ -55,19 +55,29 @@ module Test_simplify = struct
       (Var "x", {| x ^ 1 |});
       (Const 0, {| x - - - x |});
       (Const 15, {| (0 * x + 1) * 3 + 12 |});
+      (Const 7, {| 1 + 2 * 3 |});
+      (Const 21, {| (1 + 2) * (3 + 4) |});
+      (Const 15, {| (0 * x + 1) * 3 + 12 |});
+      (Const 0, {| 0 + (0 + (1 - 1)) |});
+      (Const 0, {| z * (0 * (x * y)) |});
+      (Const 8, {| 2 ^ (1 + 2) |});
+      (Var "x", {| 5 + (x - 5) |});
+      (Var "x", {| 3 + ((x - 1) - 2) |});
+      (Var "x", {| ((x * 1) + 0) - ((y - y) * z) |});
+      (Const 1, {| 1 + ((x - x) * (y + z)) |});
     ]
 
-  let generate (inputs : (Syntax.t * string) list) =
+  let generate ?(is = inputs) () =
     let f (output, input) =
       let expected = Some output in
       let actual = Option.map Semantics.simplify (Input.parse_string input) in
       let test () = Alcotest.(check (option syntax)) same_expr expected actual in
       Alcotest.(test_case ("Simplify " ^ String.trim input) `Quick test)
     in
-    List.map f inputs
+    List.map f is
 end
 
-module Test_count = struct
+module Test_simplify_with_count = struct
   let inputs : (Syntax.t * int * string) list =
     [
       (Var "a", 1, {| a |});
@@ -89,65 +99,55 @@ module Test_count = struct
       (Const 0, 3, {| (- - (1 - 1)) |});
     ]
 
-  let generate (inputs : (Syntax.t * int * string) list) =
+  let generate ?(is = inputs) () =
     let f (output, count, input) =
       let expected = Some (output, count) in
       let actual = Option.map Semantics.simplify_with_count (Input.parse_string input) in
       let test () = Alcotest.(check (option (pair syntax int))) same_expr expected actual in
       Alcotest.(test_case ("Simplify (with count) " ^ String.trim input) `Quick test)
     in
-    List.map f inputs
+    List.map f is
 end
 
-module Test_more = struct
-  let inputs =
+module Test_simplify_partial = struct
+  let inputs : (string * string) list =
     [
-      ("7", "1 + 2 * 3");
-      ("21", "(1 + 2) * (3 + 4)");
-      ("15", "(0 * x + 1) * 3 + 12");
-      ("0", "0 + (0 + (1 - 1))");
-      ("x + 15", "x + 15 - 12 * 0");
-      ("-x", "-(-(-(x)))");
-      ("x  + y", "0 + (x + (0 + y))");
-      ("x * y", "1 * (x * (1 * y))");
-      ("0", "z * (0 * (x * y))");
-      ("x - (y - (y - x))", "x - (y - (y - x))");
-      ("8", "2 ^ (1 + 2)");
-      ("x + 1", "(x + 0) * (1 + (y - y)) + (z ^ 0)");
-      ("x + z", "(x + 0) * (1 + (y - y)) + (z ^ 1)");
-      ("x + 3", "((((x + 1) - 1) + 2) - 2) + 3");
+      ({| x + 15 |}, {| x + 15 - 12 * 0 |});
+      ({| -x |}, {| -(-(-(x))) |});
+      ({| x  + y |}, {| 0 + (x + (0 + y)) |});
+      ({| x * y |}, {| 1 * (x * (1 * y)) |});
+      ({| x - (y - (y - x)) |}, {| x - (y - (y - x)) |});
+      ({| x + 1 |}, {| (x + 0) * (1 + (y - y)) + (z ^ 0) |});
+      ({| x + z |}, {| (x + 0) * (1 + (y - y)) + (z ^ 1) |});
+      ({| x + 3 |}, {| ((((x + 1) - 1) + 2) - 2) + 3 |});
       (* Tests for c1 + (x - c2) -> x when c1 == c2 *)
-      ("x", "5 + (x - 5)");
-      ("y + 3", "7 + ((y + 3) - 7)");
+      ({| y + 3 |}, {| 7 + ((y + 3) - 7) |});
       (* Tests for c1 - (x + c2) -> -x when c1 == c2 *)
-      ("-z", "4 - (z + 4)");
-      ("-(a * b)", "10 - ((a * b) + 10)");
+      ({| -z |}, {| 4 - (z + 4) |});
+      ({| -(a * b) |}, {| 10 - ((a * b) + 10) |});
       (* More complex nested cases *)
-      ("x", "3 + ((x - 1) - 2)");
-      ("-y", "5 - ((3 + (y + 2)))");
-      ("x * (y + z)", "x * (y + (z * (2 - 1))) + (0 * w)");
-      ("x * y", "(x * (y + 0)) + (0 * z)");
-      ("x * y", "x * (y ^ ((0 + 2) - 1))");
-      ("x", "((x * 1) + 0) - ((y - y) * z)");
-      ("1", "1 + ((x - x) * (y + z))");
+      ({| -y |}, {| 5 - ((3 + (y + 2))) |});
+      ({| x * (y + z) |}, {| x * (y + (z * (2 - 1))) + (0 * w) |});
+      ({| x * y |}, {| (x * (y + 0)) + (0 * z) |});
+      ({| x * y |}, {| x * (y ^ ((0 + 2) - 1)) |});
     ]
 
-  let generate (inputs : (string * string) list) =
-    let f (expected_str, actual_str) =
-      let expected = Input.parse_string expected_str in
-      let actual = Option.map Semantics.simplify (Input.parse_string actual_str) in
+  let generate ?(is = inputs) () =
+    let f (output, input) =
+      let expected = Input.parse_string output in
+      let actual = Option.map Semantics.simplify (Input.parse_string input) in
       let test () = Alcotest.(check (option syntax)) same_expr expected actual in
-      Alcotest.(test_case ("Simplify " ^ actual_str) `Quick test)
+      Alcotest.(test_case ("Simplify " ^ String.trim input) `Quick test)
     in
-    List.map f inputs
+    List.map f is
 end
 
 let intro_tests =
   [
-    ("Test_parse", Test_parse.(generate inputs));
-    ("Test_simplify", Test_simplify.(generate inputs));
-    ("Test_count", Test_count.(generate inputs));
-    ("Test_more", Test_more.(generate inputs));
+    ("Test_parse_string", Test_parse_string.generate ());
+    ("Test_simplify", Test_simplify.generate ());
+    ("Test_simplify_with_count", Test_simplify_with_count.generate ());
+    ("Test_simplify_partial", Test_simplify_partial.generate ());
   ]
 
 let () = Alcotest.run "Intro" intro_tests
