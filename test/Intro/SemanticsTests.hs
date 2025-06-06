a8abf22cd2d62e895b542a4c9e29c48a1a816206
Henry Till <henrytill@gmail.com>
2025-06-05 22:34:32 -0700

    Intro.SemanticsTest: tweak

--- test/Intro/SemanticsTests.hs
+++ test/Intro/SemanticsTests.hs
@@ -12,29 +12,23 @@ import Prelude hiding (lex)
 parse :: String -> Expression
 parse = parseIntro . lex alexStartPos
 
-parseSimplify :: String -> Expression
-parseSimplify = simplify . parse
-
-parseSimplifyWithCount :: String -> (Expression, Int)
-parseSimplifyWithCount = simplifyWithCount . parse
-
 simplifyTests :: TestTree
 simplifyTests =
   testGroup "Semantics.simplify" $ fmap f Data.simplify
   where
     f :: (Expression, String) -> TestTree
-    f (output, input) = testCase input $ parseSimplify input @?= output
+    f (output, input) = testCase input $ simplify (parse input) @?= output
 
 simplifyPartialTests :: TestTree
 simplifyPartialTests =
   testGroup "Semantics.simplify (partial)" $ fmap f Data.simplifyPartial
   where
     f :: (String, String) -> TestTree
-    f (output, input) = testCase input $ parseSimplify input @?= parse output
+    f (output, input) = testCase input $ simplify (parse input) @?= parse output
 
 simplifyWithCountTests :: TestTree
 simplifyWithCountTests =
   testGroup "Semantics.simplifyWithCount" $ fmap f Data.simplifyWithCount
   where
     f :: (Expression, Int, String) -> TestTree
-    f (output, count, input) = testCase input $ parseSimplifyWithCount input @?= (output, count)
+    f (output, count, input) = testCase input $ simplifyWithCount (parse input) @?= (output, count)
